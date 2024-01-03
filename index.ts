f26fab30594455896d36ecf5768690433149c053
Aashutosh-922 <karaleash462@gmail.com>
2024-01-03 23:21:27 +0530

    cmmt-sad

--- index.ts
+++ index.ts
@@ -1,33 +1,64 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import adminRoutes from './src/routes/admin'; // Update the import path
-import studentRoutes from './src/routes/student'; // Update the import path
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import adminRoutes from './src/routes/admin'; // Update the import path
+// import studentRoutes from './src/routes/student'; // Update the import path
+
+// dotenv.config();
+
+// const mongoURI: string = process.env.MONGO_URI || 'default-mongodb-uri';
+// const port: number = parseInt(process.env.PORT as string, 10) || 5001;
+
+
+
+// const connectionParams: mongoose.ConnectOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
+
+
+// const app = express();
+
+// app.use(express.json());
+
+// mongoose.connect(mongoURI, connectionParams).then(() => {
+//   console.info('Connected to MongoDB');
+// });
+
+// app.use('/admin', adminRoutes);
+// app.use('/student', studentRoutes);
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on http://localhost:${port}`);
+// });
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+import adminRoutes from './src/routes/admin'; // Correct the import path
+import studentRoutes from './src/routes/student'; // Correct the import path
 
 dotenv.config();
 
-const mongoURI: string = process.env.MONGO_URI || 'default-mongodb-uri';
-const port: number = parseInt(process.env.PORT as string, 10) || 5001;
+const mongoURI = process.env.MONGO_URI || "default-mongodb-uri";
+const port = process.env.PORT || 5001;
 
-
-
-const connectionParams: mongoose.ConnectOptions = {
+const connectionParams = {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 };
-
 
 const app = express();
 
 app.use(express.json());
 
 mongoose.connect(mongoURI, connectionParams).then(() => {
-  console.info('Connected to MongoDB');
+  console.info("connected");
 });
 
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
